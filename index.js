57475e026730e1522f504948a41d76a11816eb15
rohitranjanrax <2022501361.rohit@ug.sharda.ac.in>
2025-06-18 10:30:13 +0530

    Pushing existing backend bookstore project

--- index.js
+++ index.js
@@ -1,47 +1,31 @@
 import express from "express";
-import mongoose from "mongoose";
+import mongoose, { SchemaTypeOptions } from "mongoose";
 import cors from "cors";
+import Book from "./models/bookModel.js";
 import dotenv from 'dotenv';
-import Book from './models/bookModel.js';
-import bookRoutes from'./routes/bookRoutes.js';
-
+import bookRoutes from './routes/bookRoutes.js';
 dotenv.config();
-//middlewares
+
+// Middlewares
 const app = express();
-app.use(express.json());// use to manage fronted and backend communication and app is a server
+app.use(express.json());
 
-//router
+// Routes
 app.use('/book',bookRoutes);
-app.get('/',(req,res)=>{ //  ()=>{} call back function
-    res.send("This is my first Backend Page");
+app.get('/',(req,res)=>{
+    res.send("This is my First Backend Page");
 })
-// app.post('/addbook', async (req,res)=>{
-//     const {title,author,genre,publishedDate} = req.body;
 
-//     const newBook = new Book({title,author,genre,publishedDate});
-//     try{
-//         await newBook.save();
 
-//         // res.status(201).send("Book is added successfully");
-//         res.status(201).json({message:"Book is added successfully"})
-//     }
-    
-//     catch(error){
-//         // res.status(400).send("Book Couldnot be added");
-//         res.status(400).json({message:"Book Couldnot be added",error:error.message});
-//     }
-// })
 // DB Connection and Server Startup
 mongoose.connect(process.env.MONGODB_URI)
 .then(()=>{
     console.log("Database connected successfully");
     app.listen(process.env.PORT,(err)=>{
-    if(err) {
-        console.log("server could not run");
-    }
-    console.log("My server is running on port 5500 fine");
+        if(err) console.log("Server couldnot run");
+        console.log("My server is running fine");
     })
 })
-.catch(()=>{
-    console.log("Database connection failed",err);
+.catch((err)=>{
+    console.log("Db couldnotbe connected",err);
 })
--- models/bookModel.js
+++ models/bookModel.js
@@ -6,7 +6,7 @@ const bookSchema = new mongoose.Schema(
             type: String,
             required: true,
 
-        },
+        }, 
         author:{
             type: String,
             required: true,
